b15f91a8b808d96a63a02f78246fd22ab018439b
AqueleTaldoGabo <57024740015@unicentro.edu.br>
2026-02-16 20:35:23 -0300

    Tem fades 😎

--- Source/menu.gd
+++ Source/menu.gd
@@ -1,8 +1,11 @@
 extends VBoxContainer
 
+
 const CENA_1 = preload("uid://mq5h3p2in5jw")
 
 func _on_botao_start_pressed() -> void:
+	Transicao.transicao()
+	await Transicao.fade_acabou
 	get_tree().change_scene_to_packed(CENA_1)	
 
 func _on_botao_sair_pressed() -> void:
