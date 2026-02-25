bfba487faf338588845383dd59a7403684614f1d
AqueleTaldoGabo <57024740015@unicentro.edu.br>
2026-02-25 11:27:37 -0300

    Organizado

--- Source/Global/Scripts/mudarScena.gd
+++ Source/Global/Scripts/mudarScena.gd
@@ -3,9 +3,9 @@ extends Node
 const MUSICA1 = preload("res://Source/Assets/Música/menucarimbooficial.ogg")
 const MUSICA2 = preload("res://Source/Assets/Música/menucarimbador.ogg")
 const MUSICA3 = preload("res://Source/Assets/Música/temadagameplaymixado2.mp3")
-@onready var Cafeteria = preload("res://Source/Scenes/Levels/Cafeteria.tscn")
-@onready var Cena1 = preload("res://Source/Scenes/Levels/Cena1.tscn")
-@onready var Menu = preload("res://Source/Scenes/Levels/Menu.tscn")
+@onready var Cafeteria = preload("res://Source/Scenes/Levels/Cenas/Cafeteria.tscn")
+@onready var Cena1 = preload("res://Source/Scenes/Levels/Cenas/Cena1.tscn")
+@onready var Menu = preload("res://Source/Scenes/Levels/Cenas/Menu.tscn")
 
 func mudarCafeteria():
 	Transicao.transicao()
