f3990936c09371a9a96648398c2f7336a4b1661b
Siulhk <siullhk2@gmail.com>
2026-02-25 20:37:43 -0300

    '-'
    🤠

---
+++ Source/Scenes/Objects/Cenas/multi_mesh_instance_3d.gd
@@ -0,0 +1,20 @@
+extends MultiMeshInstance3D
+
+var vetores = [Vector3(-16.252, 1.06, -5.569), Vector3(-17.962, 1.06, -5.494), Vector3(-18.831, 1.06, -5.553), 
+				Vector3(-20.513, 1.06, -5.494)]
+var lado = true
+
+
+func _ready() -> void:
+	for i in 4:
+		var deixa_horizontal
+		if lado == true:
+			deixa_horizontal = Basis(Vector3.UP, deg_to_rad(-90))
+			lado = false
+		else :
+			deixa_horizontal = Basis(Vector3.UP, deg_to_rad(90))
+			lado = true
+		var final = deixa_horizontal
+		final = final.scaled(Vector3(4, 25, 24))
+		print(i)
+		multimesh.set_instance_transform(i, Transform3D(final, vetores[i]))
---
+++ Source/Scenes/Objects/Cenas/multi_mesh_instance_3d_2.gd
@@ -0,0 +1,14 @@
+extends MultiMeshInstance3D
+
+var vetores = [Vector3(-1.997, 0.022, 9.874), Vector3(-5.933, 0.022, 9.874), Vector3(-10.007, 0.022, 9.874)]
+var lado = true
+
+
+func _ready() -> void:
+	var deixa_vertical = Basis(Vector3.RIGHT, deg_to_rad(-90))
+	for i in 3:
+		
+		var final = deixa_vertical
+		final = final.scaled(Vector3(22, 22, 22))
+		print(i)
+		multimesh.set_instance_transform(i, Transform3D(final, vetores[i]))
